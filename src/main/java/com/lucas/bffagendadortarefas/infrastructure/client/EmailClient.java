2eb5b0ae77351f5c18557f9cf8cc88dd60f24fdb
Lucas <lucass3001@gmail.com>
2025-09-26 11:58:30 -0300

    adicionado Cron

--- src/main/java/com/lucas/bffagendadortarefas/infrastructure/client/EmailClient.java
+++ src/main/java/com/lucas/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -3,10 +3,13 @@ package com.lucas.bffagendadortarefas.infrastructure.client;
 
 import com.lucas.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
+
+    @PostMapping
     void enviarEmail(@RequestBody TarefasDTOResponse dto);
 }
 
